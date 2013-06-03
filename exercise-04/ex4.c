d8e068f4a8d8df8b6c3577d71530bf22a816cd7b
Eli Gundry <eligundry@gmail.com>
2013-06-03 10:20:12 -0400

    Completed exercise 4

---
+++ exercise-04/ex4.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ex3.c
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  06/03/2013 09:45:01 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Eli Gundry
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+int main()
+{
+	int age = 22,
+		height = 71;
+
+	printf("I am %d years old.\n", age);
+	printf("I am %d inches tall.\n", height);
+
+	return 0;
+}
