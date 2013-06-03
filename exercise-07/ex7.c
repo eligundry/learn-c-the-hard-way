654b538529e5a645874187afdff7b404ae2da3fd
Eli Gundry <eligundry@gmail.com>
2013-06-03 10:41:41 -0400

    Completed exercise 7

---
+++ exercise-07/ex7.c
@@ -0,0 +1,41 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ex7.c
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  06/03/2013 10:35:18 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Eli Gundry
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	int bugs = 100;
+	double bug_rate = 1.2;
+
+	printf("You have %d bugs at the imaginary rate of %f.\n",
+			bugs, bug_rate);
+
+	long universe_of_defects = 1L * 1024L * 1024L * 1024L;
+	printf("The entire universe has %ld bugs!\n", universe_of_defects);
+
+	double expected_bugs = bugs * bug_rate;
+	printf("You are expected to have %f bugs!\n", expected_bugs);
+
+	double part_of_universe = expected_bugs / universe_of_defects;
+	printf("That is only a %e portion of the universe!\n", part_of_universe);
+
+	char nul_byte = '\0';
+	int care_percent = bugs * nul_byte;
+	printf("Which means you should care %d%%.\n", care_percent);
+
+	return 0;
+}
