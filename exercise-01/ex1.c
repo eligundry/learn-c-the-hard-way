18867a0d9b40907df7734156845fdc4abcfd3d31
Eli Gundry <eligundry@gmail.com>
2013-06-03 09:35:30 -0400

    Completed exercise 1

---
+++ exercise-01/ex1.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ex1.c
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  06/03/2013 09:32:33 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Eli Gundry
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	puts("Hello world!");
+
+	return 0;
+}
