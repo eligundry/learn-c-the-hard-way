1e0fffc5afa5e7a93c72cd20e274141751631092
Eli Gundry <eligundry@gmail.com>
2013-06-03 10:23:53 -0400

    Completed exercise 5

---
+++ exercise-05/ex5.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ex5.c
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  06/03/2013 10:22:27 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Eli Gundry
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	int distance = 100;
+
+	printf("You are %d mile away!\n", distance);
+
+	return 0;
+}
