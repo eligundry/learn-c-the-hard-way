4ed3e15277aef0870784726ec8e2877386239e0d
Eli Gundry <eligundry@gmail.com>
2013-06-03 11:04:41 -0400

    Started exercise 9.

---
+++ exercise-09/ex9.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ex9.c
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  06/03/2013 11:03:06 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Eli Gundry
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	int numbers[4] = { 0 };
+	int name[4] = { 'a' };
+	return 0;
+}
